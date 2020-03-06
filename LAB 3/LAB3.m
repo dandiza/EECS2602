9ab57dd82cb21d0f2e550d265358919ecfdb4bd7
mikepsl <mackdiza@gmail.com>
2020-03-06 13:11:56 -0500

    MAR 6
    E1PartB done

--- LAB 3/LAB3.m
+++ LAB 3/LAB3.m
@@ -21,4 +21,20 @@ ylabel("X(t)");
 
 % B)
 
+n = -10:1:10;
+D_n = (4/pi)./(1-4*n.^2);
+phase = angle(D_n);
+mag = abs(D_n);
+figure(2)
+subplot(2,1,1)
+stem(n, mag);
+xlabel("n");
+ylabel("Magnitude");
+subplot(2,1,2)
+stem(n, phase);
+xlabel("n");
+ylabel("Phase");
+
+
+
 
